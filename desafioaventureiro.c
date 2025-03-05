58f699b30b3e28a242f16630b9523c990ee97c35
maxwellsousalima <maxwellsousa208@gmail.com>
2025-03-05 20:35:27 +0000

    desafioaventureiro

--- desafioaventureiro.c
+++ desafioaventureiro.c
@@ -8,6 +8,7 @@ int populacao , pontosturisticos;
 float area, pib;
 char pais[30];
 float densidade;
+int opcao;
 
 // criando variaveis carta 2
 
@@ -44,7 +45,7 @@ printf("pontos turisticos:%d\n", pontosturisticos);
 
 densidade = populacao / area;
 
-printf("densidade populacional: %f\n", densidade);
+printf("densidade populacional: %f\n\n", densidade);
 
 printf("digite o pais:\n");
 scanf("%s", &pais2);
@@ -74,40 +75,108 @@ densidade2 = populacao2 / area2;
 
 printf("densidade populacional:%f\n", densidade2);
 
-// menu dos atributos das cartas para mostrar na tela os atributos comparados
+// menu dos atributos das cartas para usuario escolher qual atributo comparar
 
-printf("comparando cartas por populacao\n");
-switch (populacao)
+printf("\nMENU\n");
+printf("\nESCOLHA QUAL ATRIBUTO COMPARAR\n");
+printf("1.populacao\n");
+printf("2.area\n");
+printf("3.pib\n");
+printf("4.pontos turisticos\n");
+printf("5.densidade\n");
+scanf("%d", &opcao);
+
+switch (opcao)
 {
 case 1:
-    printf("populacao carta 1:\n%d", populacao);
-    break;
-}
-switch (populacao2)
+    printf("comparando cartas por populacao\n");
+    if (populacao == populacao2)
 {
-case 1:
-    printf("populcacao carta 2:\n%d", populacao2);
-    break;
-} 
-// logica para comparar qual carta vence
-if (populacao == populacao2)
-{ 
-    printf("empate!!!");
+    printf("\nempate\n");
 } else if (populacao > populacao2)
 {
-    printf("carta 1 venceu\n");
-    printf("pais 1:%s\n", pais);
-    printf("populacao:%d\n", populacao);
+    printf("\ncarta um venceu\n");
+} else{
+    printf("\ncarta dois venceu\n");
+}
+printf("pais: %s\n", pais);
+printf("pais 2: %s\n", pais2);
+printf("populacao: %d\n", populacao);
+printf("populacao 2: %d\n", populacao2);
+   break;
+
+case 2:
+printf("comparando cartas por area\n");
+if (area == area2)
+{
+    printf("\nempate\n");
+} else if (area > area2)
+{
+    printf("\ncarta um venceu\n");
+} else{
+    printf("\ncarta dois venceu\n");
+} 
+printf("pais: %s\n", pais);
+printf("pais 2: %s\n", pais2);
+printf("area: %f\n", area);
+printf("area 2: %f\n", area2);
+break;
+
+case 3:
+printf("comparando cartas por pib\n");
+if (pib == pib2)
+{
+    printf("\nempate\n");
+} else if (pib > pib2)
+{
+    printf("\ncarta um venceu\n");
+} else{
+    printf("\ncarta dois venceu\n");
+} 
+printf("pais: %s\n", pais);
+printf("pais 2: %s\n", pais2);
+printf("pib: %f\n", pib);
+printf("pib 2: %f\n", pib2);
+break;
+case 4:
+printf("comparando cartas por pontos turisticos\n");
+if (pontosturisticos == pontosturisticos2)
+{
+    printf("\nempate\n");
+} else if (pontosturisticos > pontosturisticos2)
+{
+    printf("\ncarta um venceu\n");
+} else{
+    printf("\ncarta dois venceu\n");
+} 
+printf("pais: %s\n", pais);
+printf("pais 2: %s\n", pais2);
+printf("pontos turisticos: %d\n", pontosturisticos);
+printf("pontos turisticos 2: %d\n", pontosturisticos2);
+break;
+case 5:
+printf("comparando cartas por densidade\n");
+if (densidade == densidade2)
+{
+    printf("\nempate\n");
 } else if (densidade < densidade2)
 {
-printf("carta 1 venceu por densidade\n");
-printf("pais 1:%s\n", pais);
-printf("populacao:%d\n", populacao);
-} else {
-    printf("carta 2 venceu\n");
-    printf("pais %s\n", pais2);
-    printf("populacao:%d\n", populacao2);
+    printf("\ncarta um venceu\n");
+} else{
+    printf("\ncarta dois venceu\n");
+} 
+printf("pais: %s\n", pais);
+printf("pais 2: %s\n", pais2);
+printf("densidade: %f\n", densidade);
+printf("densidade 2: %f\n", densidade2);
+break;
+default:
+printf("selecione um opcao valida\n");
+    break;
 }
+
+
+
 
 
 
